ccaf49aca502ad59880cbe8c6da156531b070e37
Unmixeddiplomacy <147068106+Unmixeddiplomacy@users.noreply.github.com>
2025-07-25 23:05:35 +0530

    dev

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
