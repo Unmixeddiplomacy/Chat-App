c02ee0c63ab688684c524de51c5f5616881ccebd
Unmixeddiplomacy <147068106+Unmixeddiplomacy@users.noreply.github.com>
2025-07-30 23:48:12 +0530

    render

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://chat-app-tcgb.vercel.app",
+    origin: "*",
     credentials: true
 }))
 
